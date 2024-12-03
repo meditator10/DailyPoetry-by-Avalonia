44b068dc7e3f00c3bdba49fe1c1493c992a440d8
meditator10 <3468257309@qq.com>
2024-12-03 16:16:59 +0800

    添加今日诗词在本地数据库查询功能

--- Dpa.Library/ViewModels/TodayDetailViewModel.cs
+++ Dpa.Library/ViewModels/TodayDetailViewModel.cs
@@ -7,6 +7,14 @@ namespace Dpa.Library.ViewModels;
 
 public class TodayDetailViewModel : ViewModelBase
 {
+    private readonly IMenuNavigationService _menuNavigationService;
+
+    public TodayDetailViewModel(IMenuNavigationService menuNavigationService)
+    {
+        _menuNavigationService = menuNavigationService;
+        QueryCommand = new RelayCommand(Query);
+    }
+
     private TodayPoetry _todayPoetry;
 
     public TodayPoetry TodayPoetry
@@ -19,4 +27,9 @@ public class TodayDetailViewModel : ViewModelBase
     {
         TodayPoetry = parameter as TodayPoetry;
     }
+
+    public ICommand QueryCommand { get; }
+
+    public void Query() =>
+        _menuNavigationService.NavigateTo(MenuNavigationConstant.QueryView);
 }
